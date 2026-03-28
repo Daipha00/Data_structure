de14f9083417477409b72bb543bbc7425797482a
Daipha00 <msagaladaines@gmail.com>
2026-03-28 12:21:28 +0100

    Competent practice

---
+++ Duplicate.java
@@ -0,0 +1,31 @@
+//Given an array of integers, return true if any value appears at least twice in the 
+// array, and return false if every element is distinct.
+
+import java.util.HashSet;
+
+public class Duplicate {
+    public static void main(String[] args) {
+        int[] nums = {1,4,3,5,4};
+
+        Duplicate duplicate = new Duplicate();
+        boolean isDuplicate = duplicate.containesDuplicate(nums);
+
+        System.out.println(isDuplicate);
+    }
+
+    public boolean containesDuplicate(int[] nums){
+    HashSet<Integer> presentNumbers = new HashSet<>();
+
+    
+    for (int num: nums){
+        if (presentNumbers.contains(num)){
+            return true;
+        }
+        presentNumbers.add(num);
+    }
+
+    return false;
+    }
+
+
+}

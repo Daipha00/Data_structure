4dc1c847cd73e361c83507c9f500cbcdcb82b741
Daipha00 <msagaladaines@gmail.com>
2026-04-12 23:40:09 +0200

    fourth commit

---
+++ BestTimeToBuySellStock.java
@@ -0,0 +1,25 @@
+public class BestTimeToBuySellStock {
+
+    public static int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices1 = {7, 1, 5, 3, 6, 4};
+        int[] prices2 = {7, 6, 4, 3, 1};
+
+        System.out.println(maxProfit(prices1)); // 5
+        System.out.println(maxProfit(prices2)); // 0
+    }
+}
---
+++ ContainsDuplicate.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 1};
+        int[] nums2 = {1, 2, 3, 4};
+
+        System.out.println(containsDuplicate(nums1)); // true
+        System.out.println(containsDuplicate(nums2)); // false
+    }
+}
